718f56ee48fcc6ee8c85de2702e63778b630f135
mgorkiewicz <c-mgorkiewicz@xpansiv.com>
2023-12-12 11:45:56 +0100

    part 2 not resolved

--- src/main/kotlin/Day10PipeMaze.kt
+++ src/main/kotlin/Day10PipeMaze.kt
@@ -1,4 +1,5 @@
 import data.Day10Point
+import kotlin.math.abs
 
 class Day10PipeMaze {
 
@@ -15,11 +16,23 @@ class Day10PipeMaze {
     private val PIPES = setOf(NS, EW, NE, NW, SW, SE)
 
     private val DEFAULT_DISTANCE = -1
+    private val INSIDE_LOOP = -2
 
     fun getDistanceToFurthestPosition(pipesSketch: List<String>): Int {
         val pipesMap = pipesSketch.map { it.toCharArray() }
         val distancesMap = pipesMap.traversePipeLoop()
         return distancesMap.findMax()
+    }
+
+    fun countSpacesInsideLoop(pipesSketch: List<String>): Int {
+        val pipesMap = pipesSketch.map { it.toCharArray() }
+        val distancesMap = pipesMap.traversePipeLoop(true)
+        distancesMap.draw()
+        distancesMap.markInnerAndOuterLoops()
+        println()
+        println()
+        distancesMap.draw()
+        return distancesMap.countSpacesInside()
     }
 
     private fun List<CharArray>.findStartingPoint(): Day10Point {
@@ -29,25 +42,30 @@ class Day10PipeMaze {
         return Day10Point(-1, -1)
     }
 
-    private fun List<CharArray>.traversePipeLoop(): List<IntArray> {
+    private fun List<CharArray>.traversePipeLoop(onePath: Boolean = false): List<IntArray> {
         val distancesMap = this.map { IntArray(it.size).map { DEFAULT_DISTANCE }.toIntArray() }
         val startingPoint = this.findStartingPoint()
         this.replaceStartingPoint(startingPoint)
         distancesMap[startingPoint.y][startingPoint.x] = 0
         val connectedPipes = this.getConnectedPipes(startingPoint, startingPoint)
         for (connectedPipe in connectedPipes) {
-            this.traversePipeLoopHelper(startingPoint, startingPoint, connectedPipe, distancesMap, 1)
+            this.traversePipeLoopHelper(startingPoint, startingPoint, connectedPipe, distancesMap, 1, onePath)
         }
 
         return distancesMap
     }
 
-    private tailrec fun List<CharArray>.traversePipeLoopHelper(startingPoint: Day10Point, previous: Day10Point, currentPoint: Day10Point, distancesMap: List<IntArray>, currentDistance: Int){
+    private tailrec fun List<CharArray>.traversePipeLoopHelper(startingPoint: Day10Point, previous: Day10Point, currentPoint: Day10Point, distancesMap: List<IntArray>, currentDistance: Int, onePath: Boolean){
         if (startingPoint == currentPoint) return
-        if (distancesMap[currentPoint.y][currentPoint.x] > currentDistance || distancesMap[currentPoint.y][currentPoint.x] == DEFAULT_DISTANCE) distancesMap[currentPoint.y][currentPoint.x] = currentDistance
+        if (onePath){
+            if (distancesMap[currentPoint.y][currentPoint.x] == DEFAULT_DISTANCE) distancesMap[currentPoint.y][currentPoint.x] = currentDistance
+        } else {
+            if (distancesMap[currentPoint.y][currentPoint.x] > currentDistance || distancesMap[currentPoint.y][currentPoint.x] == DEFAULT_DISTANCE) distancesMap[currentPoint.y][currentPoint.x] = currentDistance
+        }
+
         val connectedPipes = this.getConnectedPipes(previous, currentPoint)
         for (connectedPipe in connectedPipes) {
-            return traversePipeLoopHelper(startingPoint, currentPoint, connectedPipe, distancesMap, currentDistance + 1)
+            return traversePipeLoopHelper(startingPoint, currentPoint, connectedPipe, distancesMap, currentDistance + 1, onePath)
         }
         return
     }
@@ -123,5 +141,82 @@ class Day10PipeMaze {
             if (line.max() > max) max = line.max()
         }
         return max
+    }
+
+    private fun List<IntArray>.markInnerAndOuterLoops() {
+        for ((y, line) in this.withIndex()) {
+            for ((x, symbol) in line.withIndex()) {
+                if (this[y][x] == DEFAULT_DISTANCE) {
+                    if (y == 0 || x == 0 || y == this.lastIndex || x == this[y].lastIndex ) continue
+                    val xProgressions = listOf( x - 1 downTo 0, x + 1 .. this[y].lastIndex)
+                    val yProgressions = listOf( y - 1 downTo 0, y + 1 .. this.lastIndex)
+                    val xRayProjections = xProgressions.map { this.castRayX(y, x, it) }
+                    val yRayProgressions = yProgressions.map { this.castRayY(y, x, it) }
+                    if (xRayProjections.contains(false) || yRayProgressions.contains(false)) continue
+                    this[y][x] = INSIDE_LOOP
+                }
+            }
+        }
+    }
+
+    private fun List<IntArray>.castRayX(y: Int, x: Int, xProgression: IntProgression): Boolean {
+        var intersections = 0
+        var previousValue = this[y][x]
+        var continuousValue = false
+        for (curX in xProgression) {
+            if (this[y][curX] > DEFAULT_DISTANCE) {
+                if ((abs(previousValue - this[y][curX]) == 1) || (previousValue == 0 || this[y][curX] == 0)) { //continuous value
+                    if (!continuousValue) {
+                        continuousValue = true
+                        ++intersections
+                    }
+                } else {
+                    ++intersections
+                    continuousValue = false
+                }
+                previousValue = this[y][curX]
+            }
+        }
+        return (intersections % 2 != 0)
+    }
+
+    private fun List<IntArray>.castRayY(y: Int, x: Int, yProgression: IntProgression): Boolean {
+        var intersections = 0
+        var previousValue = this[y][x]
+        var continuousValue = false
+        for (curY in yProgression) {
+            if (this[curY][x] > DEFAULT_DISTANCE) {
+                if ((abs(previousValue - this[curY][x]) == 1) || (previousValue == 0 || this[curY][x] == 0)){ //continuous value
+                    if (!continuousValue) {
+                        continuousValue = true
+                        ++intersections
+                    }
+                } else {
+                    ++intersections
+                    continuousValue = false
+                }
+                previousValue = this[curY][x]
+            }
+        }
+        return (intersections % 2 != 0)
+    }
+
+    private fun List<IntArray>.draw() {
+        for ((y, line) in this.withIndex()) {
+            for ((x, symbol) in line.withIndex()) {
+                print(symbol.toString() + "\t")
+            }
+            println()
+        }
+    }
+
+    private fun List<IntArray>.countSpacesInside(): Int {
+        var spacesInsideCount = 0
+        for ((y, line) in this.withIndex()) {
+            for ((x, symbol) in line.withIndex()) {
+                if (this[y][x] == INSIDE_LOOP ) ++spacesInsideCount
+            }
+        }
+        return spacesInsideCount
     }
 }
--- src/test/kotlin/Day10PipeMazeTest.kt
+++ src/test/kotlin/Day10PipeMazeTest.kt
@@ -45,4 +45,81 @@ class Day10PipeMazeTest {
         val result = day10PipeMaze.getDistanceToFurthestPosition(input)
         assertEquals(6909, result)
     }
+
+    @Test
+    fun countSpacesInsideLoopExample() {
+        val input = """
+            ...........
+            .S-------7.
+            .|F-----7|.
+            .||.....||.
+            .||.....||.
+            .|L-7.F-J|.
+            .|..|.|..|.
+            .L--J.L--J.
+            ...........
+        """.trimIndent().split("\n")
+        val result = day10PipeMaze.countSpacesInsideLoop(input)
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun countSpacesInsideLoopExample2() {
+        val input = """
+            ..........
+            .S------7.
+            .|F----7|.
+            .||....||.
+            .||....||.
+            .|L-7F-J|.
+            .|..||..|.
+            .L--JL--J.
+            ..........
+        """.trimIndent().split("\n")
+        val result = day10PipeMaze.countSpacesInsideLoop(input)
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun countSpacesInsideLoopExample3() {
+        val input = """
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+        """.trimIndent().split("\n")
+        val result = day10PipeMaze.countSpacesInsideLoop(input)
+        assertEquals(8, result)
+    }
+
+    @Test
+    fun countSpacesInsideLoopExample4() {
+        val input = """
+                OF----7F7F7F7F-7OOOO
+                O|F--7||||||||FJOOOO
+                O||OFJ||||||||L7OOOO
+                FJL7L7LJLJ||LJIL-7OO
+                L--JOL7IIILJS7F-7L7O
+                OOOOF-JIIF7FJ|L7L7L7
+                OOOOL7IF7||L7|IL7L7|
+                OOOOO|FJLJ|FJ|F7|OLJ
+                OOOOFJL-7O||O||||OOO
+                OOOOL---JOLJOLJLJOOO
+        """.trimIndent().split("\n")
+        val result = day10PipeMaze.countSpacesInsideLoop(input)
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun countSpacesInsideLoop() {
+        val input = readResourceFile()
+        val result = day10PipeMaze.countSpacesInsideLoop(input)
+        assertEquals(41, result)  //wrong
+    }
 }
